2295b7253ca5f7a52c4e1582af8f3891ff8cd248
Keisuke Yabuoshi <mas08rin10yabko@gmail.com>
2025-05-09 22:37:21 +0900

    Configure Prettier which works with ESLint

--- portfolio-web/src/App.tsx
+++ portfolio-web/src/App.tsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import { Button } from './components/ui/button'
+import { useState } from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </Button>
+      <Button onClick={() => setCount(count => count + 1)}>count is {count}</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
