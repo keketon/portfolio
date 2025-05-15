5d3d1b1f154ef1db7b8d91c78c93c11bb9102967
Keisuke Yabuoshi <mas08rin10yabko@gmail.com>
2025-05-15 22:43:49 +0900

    Recreate as library

---
+++ portfolio-api/lib/src/main/java/com/keketon/profile/api/handlers/RecordScoreHandler.java
@@ -0,0 +1,20 @@
+package com.keketon.profile.api.handlers;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.keketon.profile.api.dto.RecordScoreRequest;
+import com.keketon.profile.api.dto.RecordScoreResponse;
+
+public class RecordScoreHandler implements RequestHandler<RecordScoreRequest, RecordScoreResponse> {
+
+  // private static final DynamoDBCluent dynamoDBClient = DynamoDBClient.builder()
+  //     .region(Region.US_EAST_1)
+  //     .build();
+
+  @Override
+  public RecordScoreResponse handleRequest(
+      final RecordScoreRequest request, final Context context) {
+    context.getLogger().log("Requested score is: " + request.score());
+    return new RecordScoreResponse(1);
+  }
+}
